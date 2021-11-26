edb23f905bc1a8811cbc8970285483860af138e8
Campbell-Lythe-Brown <73807815+Campbell-Lythe-Brown@users.noreply.github.com>
2021-11-26 16:49:22 +1300

    Updated with UI Test Framwork

---
+++ CryptoTrack.UITest/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CryptoTrack.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.cryptotrack").EnableLocalScreenshots().StartApp();
+            }
+            else
+            {
+                return ConfigureApp.iOS.StartApp();
+            }
+
+            
+        }
+    }
+}
---
+++ CryptoTrack.UITest/Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CryptoTrack.UITest
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void ButtonTextIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Refresh Coin Values"));
+            app.Screenshot("CryptoCurrency");
+
+            Assert.IsTrue(results.Any());
+        }
+    }
+}
